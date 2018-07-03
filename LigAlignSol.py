30c4702df5dca54c1a324fba46b2bf3cec3b9d10
Anurag Kumar <39327488+anuragkumarbioinfo@users.noreply.github.com>
2018-07-03 14:22:19 +0900

    Update LigAlignSol.py

--- LigAlignSol.py
+++ LigAlignSol.py
@@ -33,12 +33,16 @@ class Geometry:
                     lines = map(float,lines.split()[5:8])
                     self.pdb_coords.append(lines)
     
-    def calculate_COG(self):
-        x1,y1,z1 = zip(*self.pdb_coords)
-        self.pdb_COG = [sum(x1)/len(x1),sum(y1)/len(y1),sum(z1)/len(z1)]
-        
-        
+    def calculate_COG(self, final=False):
+        #print "[COG]: ", final
+        if not final: 
+            x1,y1,z1 = zip(*self.pdb_coords)
+            self.pdb_COG = [sum(x1)/len(x1),sum(y1)/len(y1),sum(z1)/len(z1)]
+        else: 
+            x1,y1,z1 = zip(*final)
+            return([sum(x1)/len(x1),sum(y1)/len(y1),sum(z1)/len(z1)])
     def write_PDB(self,outfile):
+        self.otf = []
         Nx, Ny, Nz = self.oxygen
         Ox, Oy, Oz = self.pdb_COG
         new_x = []
@@ -55,25 +59,30 @@ class Geometry:
             with open(outfile,"w") as output:
                 for lines in p1_g:
                     if lines.startswith("ATOM"):
-                        output.write(lines[:26]+'   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}\n'.format(new_x[counter], new_y[counter],new_z[counter], 0.00, 0.00))
+                        output.write(lines[:26]+'   {:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}\n'.format(new_x[counter]+11.00, new_y[counter],new_z[counter], 0.00, 0.00))
+                        self.otf.append([new_x[counter]+11.00, new_y[counter],new_z[counter]])
                         counter += 1
     
     def nearest_atom(self, coordinate,protein):
         self.coordinate = map(float,coordinate)
+        self.coords = {}
         distance_list = []
         with open(protein) as pp:
             for lines in pp:
                 if lines.startswith("ATOM"):
                     linesZ = map(float,lines.split()[5:8])
                     name = "_".join(lines.split()[1:5])
+                    self.coords[name] = linesZ
                     self.savedistance[name] = self.distance(linesZ,self.coordinate)
         rr = min(self.savedistance, key=self.savedistance.get)
+        #print self.calculate_COG(self.otf)
+        print "\n\nDistance between {} and ligand is = {:5.5} Angstrom".format(rr, self.distance(self.calculate_COG(self.otf),self.coords[rr] ))
         return rr, self.savedistance[rr]
 
 
 
 import sys
-#python LigAlignSol.py ACX.pdb 94.935  50.098  25.530 74.398  49.079  36.679 protein.pdb output.pdb
+#python LigAlignSol.py ACX.pdb tail 94.935  50.098  25.530 head 74.398  49.079  36.679 protein.pdb output.pdb
 a = sys.argv
 pdb = a[1]
 protein = a[-2]
@@ -82,5 +91,6 @@ pxyz = a[5:8]
 output = a[-1]
 query = Geometry(pdb, xyz)
 query.runscript(output)
-print query.nearest_atom(pxyz,protein)
+x,d = query.nearest_atom(pxyz,protein)
+print "Nearest Neighbour found: {} at {:5.5} Angstrom\n\n".format(x,d)
 #print query.distance([1,1,1],[0,0,0])
